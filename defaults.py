bc8c5a1e240cb82b22b3732f1ee5bcfdd8e12246
sonny <sonny@buzzni.com>
2017-09-16 20:34:29 +0900

    Add defaults for register settings

---
+++ defaults.py
@@ -0,0 +1,40 @@
+from mezzanine.conf import register_setting
+
+register_setting(
+    name="GITHUB_NAME",
+    label="github name",
+    description="",
+    editable=True,
+    default='',
+)
+
+register_setting(
+    name="FACEBOOK_NAME",
+    label="fackbook name",
+    description="",
+    editable=True,
+    default='',
+)
+
+register_setting(
+    name="LINKDIN_NAME",
+    label="linkdin name",
+    description="",
+    editable=True,
+    default='',
+)
+
+register_setting(
+    name="COPYRIGHT",
+    label="copyright name",
+    description="",
+    editable=True,
+    default='',
+)
+
+register_setting(
+    name="TEMPLATE_ACCESSIBLE_SETTINGS",
+    editable=False,
+    default=("GITHUB_NAME", "FACEBOOK_NAME", "LINKDIN_NAME", "COPYRIGHT", ),
+    append=True,
+)
